f6ba850445c0b378c0d757b9d1e0fd7a8ba4dedb
Bilibox <1800580+Bilibox@users.noreply.github.com>
2021-10-01 16:25:35 -0700

    Format Script

--- Snahp Auto Base64 Decode.user.js
+++ Snahp Auto Base64 Decode.user.js
@@ -16,79 +16,90 @@
 
 var results = [];
 
-if (document.getElementsByClassName("postbody") != undefined) {
-    CustomRecursiveTreeWalker()
-    let thread = document.getElementsByClassName("postbody")[0].getElementsByClassName("content")[0];
-    for (let index = 0; index < results.length; index++) {
-        thread.innerHTML = thread.innerHTML.replace('TextWasDecoded', results[index]);
-    }
+if (document.getElementsByClassName('postbody') != undefined) {
+	CustomRecursiveTreeWalker();
+	let thread = document
+		.getElementsByClassName('postbody')[0]
+		.getElementsByClassName('content')[0];
+	for (let index = 0; index < results.length; index++) {
+		thread.innerHTML = thread.innerHTML.replace(
+			'TextWasDecoded',
+			results[index]
+		);
+	}
 }
 
 function Base64ToUTF8(str) {
-    try {
-        return decodeURIComponent(escape(window.atob(str)));
-    } catch (e) {
-        return '';
-    }
+	try {
+		return decodeURIComponent(escape(window.atob(str)));
+	} catch (e) {
+		return '';
+	}
 }
 
 function ResultsPush(encoded, decoded) {
-    results.push(`<br>Encoded Base64: ${encoded} <br>Decoded Base64: <a href='${decoded}' class='postlink'>${decoded}</a>`)
+	results.push(
+		`<br>Encoded Base64: ${encoded} <br>Decoded Base64: <a href='${decoded}' class='postlink'>${decoded}</a>`
+	);
 }
 
 function Base64Testing(child) {
-    const base64Regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/g;
-    if (child.nodeValue.match(/(\r\n|\n|\r|\s)/gm)) {
-        var wasDecoded = false;
-        let splitText = child.nodeValue.split(/(\r\n|\n|\r|\s)/);
-        for (let index = 0; index < splitText.length; index++) {
-            if (splitText[index] && !(splitText[index].length / 4 <= 2)) {
-                let encodedText = splitText[index].match(base64Regex)
-                if (encodedText) {
-                    let decodedText = Base64ToUTF8(encodedText);
-                    if (decodedText) {
-                        if (decodedText.match(base64Regex)) {
-                            decodedText = Base64ToUTF8(decodedText)
-                        }
-                        ResultsPush(splitText[index], decodedText)
-                        splitText[index] = 'TextWasDecoded'
-                        wasDecoded = true;
-                    }
-                }
-            }
-        }
-        if (wasDecoded) {
-            child.nodeValue = splitText.join('');
-        }
-    } else {
-        let encodedText = child.nodeValue.match(base64Regex);
-        if (encodedText[0] && !(encodedText[0].length / 4 <= 2)) {
-            let decodedText = Base64ToUTF8(encodedText[0]);
-            if (decodedText) {
-                if (decodedText.match(base64Regex)) {
-                    decodedText = Base64ToUTF8(decodedText)
-                }
-                ResultsPush(child.nodeValue, decodedText)
-                child.nodeValue = 'TextWasDecoded';
-            }
-        }
-    }
+	const base64Regex =
+		/^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/g;
+	if (child.nodeValue.match(/(\r\n|\n|\r|\s)/gm)) {
+		var wasDecoded = false;
+		let splitText = child.nodeValue.split(/(\r\n|\n|\r|\s)/);
+		for (let index = 0; index < splitText.length; index++) {
+			if (splitText[index] && !(splitText[index].length / 4 <= 2)) {
+				let encodedText = splitText[index].match(base64Regex);
+				if (encodedText) {
+					let decodedText = Base64ToUTF8(encodedText);
+					if (decodedText) {
+						if (decodedText.match(base64Regex)) {
+							decodedText = Base64ToUTF8(decodedText);
+						}
+						ResultsPush(splitText[index], decodedText);
+						splitText[index] = 'TextWasDecoded';
+						wasDecoded = true;
+					}
+				}
+			}
+		}
+		if (wasDecoded) {
+			child.nodeValue = splitText.join('');
+		}
+	} else {
+		let encodedText = child.nodeValue.match(base64Regex);
+		if (encodedText[0] && !(encodedText[0].length / 4 <= 2)) {
+			let decodedText = Base64ToUTF8(encodedText[0]);
+			if (decodedText) {
+				if (decodedText.match(base64Regex)) {
+					decodedText = Base64ToUTF8(decodedText);
+				}
+				ResultsPush(child.nodeValue, decodedText);
+				child.nodeValue = 'TextWasDecoded';
+			}
+		}
+	}
 }
 
 function CustomRecursiveTreeWalker() {
-    (function findTextNodes(current) {
-        for (let i = 0; i < current.childNodes.length; i++) {
-            var child = current.childNodes[i];
-            if (child.nodeType == 3) {
-                try {
-                    Base64Testing(child)
-                } catch (e) {
-                    console.error(e)
-                }
-            }
-            else {
-                findTextNodes(child);
-            }
-        }
-    })(document.getElementsByClassName("postbody")[0].getElementsByClassName("content")[0]);
+	(function findTextNodes(current) {
+		for (let i = 0; i < current.childNodes.length; i++) {
+			var child = current.childNodes[i];
+			if (child.nodeType == 3) {
+				try {
+					Base64Testing(child);
+				} catch (e) {
+					console.error(e);
+				}
+			} else {
+				findTextNodes(child);
+			}
+		}
+	})(
+		document
+			.getElementsByClassName('postbody')[0]
+			.getElementsByClassName('content')[0]
+	);
 }
